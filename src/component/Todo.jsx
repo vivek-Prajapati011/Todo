6fab527b886d724444b19ee8c085de27f6a07ceb
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-14 22:57:43 +0530

    implelment input for adding task

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 const Todo = () => {
+    return (
+        <>
+        <div className="container">
+            <label>
+              <input className="search-bar" type="text" />
+              
+            </label>
+           
+
+
+        </div>
+        </>
+    )
 
 }
 export default Todo
